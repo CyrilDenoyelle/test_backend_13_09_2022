55457cea49cb901b89323d054c0cd130e4d6da06
Cyril Denoyelle <cyril-denoyelle@hotmail.fr>
2022-09-13 17:05:45 +0200

    use express-session

--- app/routes.js
+++ app/routes.js
@@ -4,10 +4,39 @@ const router = express.Router();
 const db = require("./db");
 
 router.get("/products", function (req, res) {
-  db.all(`SELECT * FROM products`, (err, rows) => {
+  const scope = req.session.loggedin ? "authenticated" : "public";
+  db.all(`SELECT * FROM products WHERE visible_${scope} = 1`, (err, rows) => {
     if (err) throw err;
     res.send(rows);
   });
+});
+
+router.post("/login", async function (req, res) {
+  if (!req.body.email || !req.body.password_hash) {
+    res.send({
+      payload: {},
+      status: 401,
+    });
+  }
+
+  db.get(
+    `SELECT * FROM users WHERE email = ? AND password_hash = ?`,
+    [req.body.email, req.body.password_hash],
+    (err, user) => {
+      if (err) throw err;
+
+      if (user) {
+        req.session.loggedin = true;
+        req.session.user = user;
+        res.send({ payload: {}, status: 200 });
+      } else {
+        res.send({
+          payload: {},
+          status: 401,
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
+const session = require("express-session");
 const router = require("./app/routes.js");
 
 const app = express();
 const port = 3000;
 
-app.use("/", router);
+app
+  .use(express.json())
+  .use(
+    session({
+      secret: Math.random().toString(36).substring(2, 7),
+      saveUninitialized: true,
+      resave: false,
+    })
+  )
+  .use("/", router);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
